5cc3993a26036c01adba58856987c00ae0dae893
caio cesar <alli.moreira@gmail.com>
2021-03-08 01:23:01 -0300

    Criando funcao onOperator

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -8,18 +8,48 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var lastNumeric: Boolean = false
+    var lastDot: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
     }
 
     fun onDigit(view: View){
         tvInput.append((view as Button).text)
+        lastNumeric = true
     }
 
     fun onClear(view: View){
         tvInput.text = ""
+        lastNumeric = false
+        lastDot = false
+    }
+
+    fun onDecimalPoint(view: View){
+        if(lastNumeric && !lastDot){
+            tvInput.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
+    }
+
+    fun onOperator(view: View){
+        if(lastNumeric && !isOperatorAdded(tvInput.text.toString())){
+            tvInput.append((view as Button).text)
+            lastNumeric = false
+            lastDot = false
+        }
+    }
+
+    private fun isOperatorAdded(value: String) : Boolean{
+        return if(value.startsWith("-")) {
+            false
+        }else{
+            value.contains("/") || value.contains("*")
+                    || value.contains("+") || value.contains("-")
+        }
     }
 }
